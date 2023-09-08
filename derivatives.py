fc75ec27784f17b55e24dc9d3a41c3333ddd0f84
Van-Nice <wilsonwalter808@gmail.com>
2023-09-07 20:10:27 -0600

    First Commit

---
+++ derivatives.py
@@ -0,0 +1,6 @@
+
+class derivative:
+    def __init__(self, derive_me):
+        self.deriver_me = derive_me
+
+    def derive(self):
---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+
+
+root.geometry("800x900")
+root.mainloop()
+
+
+
+if __name__ == '__main__':
+    print('PyCharm')
+
+
